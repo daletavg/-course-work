ee39391b6f418edcca70203b9f3a1833bfbe0f36
daletavg <daletavg@gmail.com>
2017-09-26 21:56:14 +0300

    add bot

--- Release/image/Tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Tileset" tilewidth="48" tileheight="48" tilecount="385" columns="35">
- <image source="Tileset.png" width="1683" height="545"/>
-</tileset>
